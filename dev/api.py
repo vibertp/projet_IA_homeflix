5c5189a3fc92c5efd36e097b0f1071b4cb1c2760
ClementMat <mathiotclement@gmail.com>
2025-03-17 20:38:04 +0100

    MAJ création du modèle

--- dev/api.py
+++ dev/api.py
@@ -3,9 +3,21 @@ import joblib
 import numpy as np
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import duckdb
+
+DB_FILE = "movies_db.duckdb"
 
 class userID(BaseModel):
     userID: int
+
+con = duckdb.connect("movies_db.duckdb")
+
+movies = con.execute("SELECT id, title FROM movies").df()
+ratings = con.execute("SELECT user_id, film_id, rating FROM ratings").df()
+model = joblib.load("model.pkl")
+
+
+app = FastAPI()
 
 @app.get("/")
 def read_root():
@@ -19,4 +31,6 @@ def read_item(userID: int):
     return {"userID": userID}
 
 @app.get("/recommend_movies")
-def 
+def recommendation(userID: int):
+    if userID not in ratings["user_id"].unique():
+        raise HTTPException(status_code=404, detail="Cet utilisateur n'existe pas")
