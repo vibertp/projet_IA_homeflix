ebce724ae86b648ecec14d15a4eab19523529d03
paul vibert <paulvibert01@gmail.com>
2025-03-25 15:22:16 +0100

    MAJ fichier graphiques

--- frontend/graphiques.py
+++ frontend/graphiques.py
@@ -1,3 +1,13 @@
-from frontend.data_loader import load_data
+#from frontend.data_loader import load_data
+import streamlit as st 
+import sys
+import os
 
-df_ratings, df_film = load_data()
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Ajouter ce dossier à sys.path pour que 'frontend' soit reconnu
+sys.path.insert(0, BASE_DIR)
+
+st.write(sys.path)
+#df_ratings, df_film = load_data()
+#st.write(df_film.head())
